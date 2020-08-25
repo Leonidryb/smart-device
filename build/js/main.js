ce296061d17eba44099c98bb10ce5ad4813a8874
leonidryb <leonidrybalkin@yandex.ru>
2020-08-25 07:44:13 +0300

    Добавляет кнопки для аккордеона, программирует его поведение

---
+++ build/js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+// Открывает и закрывает Аккордеон
+var accordions = document.querySelectorAll('.js-accordion');
+var accordionsBtn = document.querySelectorAll('.footer__btn');
+var accordionsList = document.querySelectorAll('.footer__list');
+
+accordions.forEach(function (accordion) {
+  var btn = accordion.querySelector('.footer__btn');
+  var list = accordion.querySelector('.footer__list');
+
+  btn.addEventListener('click', function () {
+    if (btn.classList.contains('footer__btn--opened')) {
+      btn.classList.remove('footer__btn--opened');
+    } else {
+      accordionsBtn.forEach(function (accordionBtn) {
+        accordionBtn.classList.remove('footer__btn--opened');
+      });
+      btn.classList.add('footer__btn--opened');
+    }
+    if (list.classList.contains('footer__list--opened')) {
+      list.classList.remove('footer__list--opened');
+    } else {
+      accordionsList.forEach(function (accordionList) {
+        accordionList.classList.remove('footer__list--opened');
+      });
+      list.classList.add('footer__list--opened');
+    }
+  });
+});
